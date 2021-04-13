e6ce1c5287fd94cfc529e5ac3349a990b52a9166
NadiriBoss <59024074+NadiriBoss@users.noreply.github.com>
2021-04-13 13:45:19 +0200

    :)

---
+++ RockPaperScissors/Controllers/GameController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class GameController : ControllerBase
+  {
+
+    [HttpPost]
+    public ActionResult<Game> CreateGame(String player1Name)
+    {
+      Game game = new Game();
+      game.gameID = new Guid();
+
+      return game;
+    }
+
+    [HttpPost("{id}/join")]
+    public ActionResult<Game> JoinGame(String player2Name)
+    {
+
+      return 
+    }
+
+    [HttpPost("{id}/move")]
+    public ActionResult<Game> PlayGame(String player2Name)
+    {
+
+      return
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Game> GameStatus()
+    {
+
+
+    }
+  }
+}
---
+++ RockPaperScissors/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Models
+{
+  public class Game
+  {
+
+    public Guid GameID { get; set; }
+    public String Status { get; set; }
+    
+    public class Player
+    {
+      public String Name { get; set; }
+
+      public String Move { get; set; }
+    }
+
+
+  }
+
+}
